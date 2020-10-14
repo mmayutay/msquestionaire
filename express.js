89681aa8ab1beb48369abf21958e35c8cce7e1d1
mmayutay <raymondjay.yorong@student.passerellesnumeriques.org>
2020-10-14 09:40:08 +0800

    Initial push

--- express.js
+++ express.js
@@ -5,6 +5,8 @@ const cors  = require("cors")
 const app = express();
 const uri = "mongodb+srv://raymondjay:iamlegendary11@cluster0.h2o1d.mongodb.net/dbCollection?retryWrites=true&w=majority";
 
+var mongoCollection = ""
+
 app.use(cors())
 app.use(express.json())
 
@@ -12,22 +14,26 @@ mongoose.connect(uri, (err, db) => {
     if (err) {
         throw err
     }else {
-        app.get('/', (req, res) => {
-            res.send("Connected to Database!")
-        })
-        console.log("Connected to Database!")
-        app.get("/allData", (req, res) => {
-            db.collection("studentsnames").find({}).toArray((err, result) => {
-                res.send(result)
-            })
-        })
-        app.post("/addAnswers", (req, res) => {
-            db.collection("studentsnames").insertOne(req.body, (err, result) => {
-                res.send(true)
-            })
-        })
+        console.log("Successfully Connected to Database!")
+        mongoCollection = db.collection("studentsnames")
     }
 })
+
+app.get('/', (req, res) => {
+    res.send("Connected to Database!")
+})
+console.log("Connected to Database!")
+app.get("/allData", (req, res) => {
+    mongoCollection.find({}).toArray((err, result) => {
+        res.send(result)
+    })
+})
+app.post("/addAnswers", (req, res) => {
+    mongoCollection.insertOne(req.body, (err, result) => {
+        res.send(true)
+    })
+})
+
 
 
 app.listen(port, () => {console.log("Listening to port "+port)})
